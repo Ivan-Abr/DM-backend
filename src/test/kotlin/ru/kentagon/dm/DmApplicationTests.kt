b5bbb82769c631fa2f978d0cbc965bfa9cfa2c5f
Ivan Abramov <ivabramov16@gmail.com>
2025-04-22 11:54:04 +0300

    DM - Инициализация проекта

---
+++ src/test/kotlin/ru/kentagon/dm/DmApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.kentagon.dm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DmApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
