67a6a1871fcf29e2ec92fbaee5363afa587c24c4
Ivan Abramov <ivabramov16@gmail.com>
2025-05-11 17:10:51 +0300

    DM3 - userCrud

---
+++ src/main/kotlin/ru/kentagon/dm/services/UserService.kt
@@ -0,0 +1,30 @@
+package ru.kentagon.dm.services
+
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+import ru.kentagon.dm.dto.user.UpdateUserDTO
+import ru.kentagon.dm.dto.user.ViewUserDTO
+import ru.kentagon.dm.models.User
+import ru.kentagon.dm.repositories.UserRepository
+import java.util.UUID
+
+@Service
+class UserService(
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder
+) {
+    fun getAllUsers() = userRepository.findAll().map { ViewUserDTO(it) }
+
+    fun getUserById(id: UUID): ViewUserDTO =
+        ViewUserDTO(userRepository.findById(id).get())
+
+    fun updateUser(id: UUID, userDTO: UpdateUserDTO): User {
+        val user = userRepository.findById(id).get()
+        userDTO.name?.let { user.name = it }
+        userDTO.email?.let { user.email = it}
+        userDTO.password?.let { user.userPassword = passwordEncoder.encode(it) }
+        return userRepository.save(user)
+    }
+
+    fun deleteUser(id: UUID) = userRepository.deleteById(id)
+}
