6c5befdcb6f584f1b2c0980c63ab18a3fde0e2a1
Ivan Abramov <ivabramov16@gmail.com>
2025-06-04 13:47:10 +0300

    DM4 - Костыль для сериализации Mark

--- src/main/kotlin/ru/kentagon/dm/models/Mark.kt
+++ src/main/kotlin/ru/kentagon/dm/models/Mark.kt
@@ -1,5 +1,6 @@
 package ru.kentagon.dm.models
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import jakarta.persistence.*
 import java.util.UUID
 
@@ -11,6 +12,7 @@ class Mark(
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "question_id", nullable = false)
+    @JsonIgnore
     var question: Question,
 
     var annotation: String,
