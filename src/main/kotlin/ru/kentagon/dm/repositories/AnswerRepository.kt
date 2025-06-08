c703269451f6153f9087d84cde6df1ea82afa397
Ivan Abramov <ivabramov16@gmail.com>
2025-06-08 15:17:18 +0300

    DM4 - Изменил вывод данных по тестированию

--- src/main/kotlin/ru/kentagon/dm/repositories/AnswerRepository.kt
+++ src/main/kotlin/ru/kentagon/dm/repositories/AnswerRepository.kt
@@ -31,14 +31,13 @@ interface AnswerRepository : JpaRepository<Answer, UUID> {
     fun getAllByFactorOrg(factorId: UUID, orgId: UUID): Any
 
     @Query(
-        "SELECT  avg(a.mark.value), l.name, f.shortname, org.name, mlst.year  from Answer AS a " +
+        "SELECT  avg(a.mark.value), l.name, org.name, mlst.year  from Answer AS a " +
                 "   LEFT JOIN Mark AS m ON a.mark.id = m.id" +
                 "   LEFT JOIN Question AS q ON m.question.id = q.id" +
-                "   LEFT JOIN Factor AS f ON q.factor.id = f.id" +
                 "   LEFT JOIN Layer AS l ON q.layer.id = l.id" +
                 "   LEFT JOIN Organization as org On a.organization.id = org.id" +
                 "   LEFT JOIN Milestone AS mlst ON a.milestone.id = mlst.id" +
                 "   WHERE org.id = ?1" +
-                "   GROUP BY l.name, org.name, f.shortname, mlst.year")
+                "   GROUP BY l.name, org.name, mlst.year")
     fun getAllByOrgId(orgId: UUID): List<Any>
 }
